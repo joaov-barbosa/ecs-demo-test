9f1895cf4513d2bceef7778700e1f7fbf0f68068
Joao Barbosa <joao.barbosa@ilegra.com>
2023-03-29 10:40:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const host = "0.0.0.0"
+const port = 3000
+const server = http.createServer(( req, res)=>{
+  res.statusCode = 200
+  res.setHeader('Content-Type','text/plain')
+  res.end('ola mundo \n meu primeiro script')
+})
+server.listen(port, host, ()=>{
+    console.log(`server running at http://${host}:${port} `)
+})
